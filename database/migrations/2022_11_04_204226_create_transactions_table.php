fe52737aeab9433762e520d68828bfe5624ad3ef
Nadachi <luiseduardort2001@gmail.com>
2022-11-04 18:01:52 -0300

    feat: adding migrations

---
+++ database/migrations/2022_11_04_204226_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('payer_id')->references('id')->on('wallets');
+            $table->foreignUuid('payee_id')->references('id')->on('wallets');
+            $table->bigInteger('amount');
+            $table->dateTime('date_transaction');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
