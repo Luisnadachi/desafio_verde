f393c67496db0fcac4787d2ca21a852b9c419440
Nadachi <luiseduardort2001@gmail.com>
2022-11-06 15:34:09 -0300

    feat: Transferencys between wallet

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,22 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TransactionRequest;
 use App\Models\Transaction;
-use Illuminate\Http\Request;
+use App\Models\Wallet;
+use Symfony\Component\HttpFoundation\Response;
 
 class TransactionController extends Controller
 {
-    public function __construct()
-    {
-
-
-    }
-
     public function pay(TransactionRequest $request)
     {
         $data = $request->validated();
-        dump($data);
+        try {
+            $payerWallet = Wallet::query()->find($data['payer_id']);
+            $payeeWallet = Wallet::query()->find($data['payee_id']);
 
-        Transaction::create($data);
+            if ($payerWallet->shopkeeper){
+                return response([
+                    'message' => 'Lojista não pode transferir!',
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            if ($payerWallet->balance < $data['amount']){
+                return response([
+                    'message' => 'Saldo insuficiente!',
+                ], Response::HTTP_NOT_ACCEPTABLE);
+            }
+
+            Wallet::query()->find($data['payer_id'])
+                ->update(['balance' => $payerWallet->balance - $data['amount']]);
+            Wallet::query()->find($data['payee_id'])
+                ->update(['balance' => $payeeWallet->balance + $data['amount']]);
+
+            Transaction::create($data);
+        } catch (\Exception $e){
+            return response()->json(['message' => $e->getMessage(), 'code' => $e->getCode()]);
+        }
     }
 
 }
--- tests/Feature/TransactionControllerTest.php
+++ tests/Feature/TransactionControllerTest.php
@@ -2,23 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Models\Shopkeeper;
 use App\Models\User;
 use App\Models\Wallet;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class TransactionControllerTest extends TestCase
 {
 
-    public function testUsuarioEfetuouPagamentoComSucesso()
+    public function testUsuarioEfetuouPagamentoComSucessoParaOutraCarteira()
     {
-        $this->withoutExceptionHandling();
         // Assign
-        $payer = User::factory()->has(Wallet::factory())->create();
+        $payer = User::factory()->has(Wallet::factory()->state(['balance' => 500]))->create();
         $payee = User::factory()->has(Wallet::factory())->create();
 
 
         $body = [
-            "amount" => 123,
+            "amount" => 200,
             "payer_id" => $payer->wallet->id,
             "payee_id" => $payee->wallet->id,
         ];
@@ -29,7 +30,68 @@ class TransactionControllerTest extends TestCase
         // Assert
         $response->assertOk();
         $this->assertDatabaseHas('transactions', $body);
+        $this->assertDatabaseHas('wallets', [
+            'id' => $payer->wallet->id,
+            'balance' => ($payer->wallet->balance - $body['amount'])
+        ]);
+
+        $this->assertDatabaseHas('wallets', [
+            'id' => $payee->wallet->id,
+            'balance' => ($payee->wallet->balance + $body['amount'])
+        ]);
     }
 
+    public function testUmLojistaTentaTransferir()
+    {
+        // Assign
+
+        $payer = Shopkeeper::factory()->has(Wallet::factory())->create();
+        $payee = User::factory()->has(Wallet::factory())->create();
+
+        $body = [
+            "amount" => 123,
+            "payer_id" => $payer->wallet->id,
+            "payee_id" => $payee->wallet->id,
+        ];
+
+        // Act
+
+        $response = $this->post(route('transaction.pay'), $body);
+
+        // Assert
+
+        $response->assertUnprocessable();
+    }
+
+    public function testUsuarioNãoTemSaldoSuficiente()
+    {
+        // Assign
+        $payer = User::factory()->has(Wallet::factory())->create();
+        $payee = User::factory()->has(Wallet::factory())->create();
+
+        $body = [
+            "amount" => 999999,
+            "payer_id" => $payer->wallet->id,
+            "payee_id" => $payee->wallet->id,
+        ];
+
+        // Act
+
+        $response = $this->post(route('transaction.pay'), $body);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_NOT_ACCEPTABLE);
+
+    }
+
+    public function testInconsistenciaNaTransacao()
+    {
+
+    }
+
+    public function testEnviadaNotificacaoParaORecebedor()
+    {
+
+    }
 
 }
